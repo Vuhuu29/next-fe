5d506f9083737c360f00d0b8c6f7e968bc1fe8e9
Vuhuu29 <thira6677@gmail.com>
2025-10-19 22:26:35 +0700

    fix middleware 2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    nodeMiddleware: true, // Bật Node.js Runtime cho middleware
+  },
 };
 
 export default nextConfig;
